20d0c64fed4bab6982e70017834dab08de8240aa
Lucas <lucasrodriguezy@gmail.com>
2023-06-19 14:57:20 -0300

    First class: Position

---
+++ chessSystem/src/board/Position.java
@@ -0,0 +1,37 @@
+package board;  
+
+public class Position {
+	private int column;
+	private int row;
+	
+	public Position(int row, int column) {
+		super();
+		this.column = column;
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	@Override
+	public String toString() {
+		return row + "," + column;
+	}
+	
+	
+	
+	
+}
